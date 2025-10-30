20f96015eba8c4ea18c6721a83911409536e7a5a
YomNom <joeyyyong@gmail.com>
2025-10-30 14:28:03 -0400

    Start

---
+++ StressReliefMiniGames/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace StressReliefMiniGames
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ StressReliefMiniGames/GlobalXmlns.cs
@@ -0,0 +1,2 @@
+[assembly: XmlnsDefinition("http://schemas.microsoft.com/dotnet/maui/global", "StressReliefMiniGames")]
+[assembly: XmlnsDefinition("http://schemas.microsoft.com/dotnet/maui/global", "StressReliefMiniGames.Pages")]
